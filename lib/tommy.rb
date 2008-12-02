8d325893285b58ec87d04372644aa1573f365101
Jeff Hodges <jeff@somethingsimilar.com>
2008-12-01 23:31:43 -0800

    tommy needs a license, a permit to hunt the big game!

--- lib/tommy.rb
+++ lib/tommy.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
 
+# This module is not part of the Divmod project, but it is Copyright 2003 Amir
+# Bakhtiar: amir@divmod.org.  This is free software; you can redistribute it
+# and/or modify it under the terms of version 2.1 of the GNU Lesser General
+# Public License as published by the Free Software Foundation.
+#
+# This ruby translation, though, was done by Jeff Hodges
+# <jeff@somethingsimilar.com>, so don't go hounding Amir about dev stuff.
 require 'set'
 
 module Tommy
